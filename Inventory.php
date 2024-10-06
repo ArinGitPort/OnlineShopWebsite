03a4b099322f94a1bfff49350ce8903c634a2319
ArinGitPort <allenportgithub@gmail.com>
2024-10-06 13:44:18 +0800

    3rd commit

--- Inventory.php
+++ Inventory.php
@@ -2,7 +2,7 @@
 <html lang="en">
 
 <head>
-    <link rel="stylesheet" href="inventorystyle.css">
+    <link rel="stylesheet" href="sidebarstyle.css">
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Inventory</title>
@@ -28,7 +28,10 @@
                     <div><a href="" class="menuDashboard">INVENTORY</a></div>
                 </li>
                 <li>
-                <div><a href="" class="menuDashboard">LOG-OUT</a></div>
+                    <div><a href="" class="menuDashboard">HOME</a></div>
+                </li>
+                <li>
+                    <div><a href="" class="menuDashboard">LOG-OUT</a></div>
                 </li>
             </ul>
         </div>
@@ -39,28 +42,21 @@
 
     <?php
     $servername = "localhost";
-    $username = "root"; // default XAMPP user
-    $password = ""; // no password by default
-    $dbname = "inventorydb";
+    $username = "root";
+    $password = "";
+    $dbname = "logindb";
 
+    // Create connection
     $conn = new mysqli($servername, $username, $password, $dbname);
 
+    // Check connection
     if ($conn->connect_error) {
         die("Connection failed: " . $conn->connect_error);
     }
 
-    if ($_SERVER['REQUEST_METHOD'] === 'POST') {
-        $item = htmlspecialchars($_POST['item']);
-        $sql = "INSERT INTO items (name) VALUES ('$item')";
-
-        if ($conn->query($sql) === TRUE) {
-            echo "Item added: " . $item;
-        } else {
-            echo "Error: " . $sql . "<br>" . $conn->error;
-        }
-    }
-
-    $conn->close();
+    // SQL query to fetch data from the items table
+    $sql = "SELECT id, productname, qty, price FROM inventory";
+    $result = $conn->query($sql);
     ?>
 
 </body>
---
+++ sidebar.php
@@ -0,0 +1,14 @@
+
+<div class="sidebarMenuContainer">
+    <ul class="listDashboard">
+        <li>
+            <div><a href="home.php" class="menuDashboard">HOME</a></div>
+        </li>
+        <li>
+            <div><a href="inventory.php" class="menuDashboard">INVENTORY</a></div>
+        </li>
+        <li>
+            <div><a href="logout.php" class="menuDashboard">LOG-OUT</a></div>
+        </li>
+    </ul>
+</div>
