d808e1a5ac357537d0c48f0bf4cd59f42534078b
ArinGitPort <allenportgithub@gmail.com>
2024-10-14 17:56:15 +0800

    12th Commit

--- home.php
+++ home.php
@@ -10,6 +10,9 @@
 </head>
 
 <body>
+
+    <?php include 'sidebar.php' ?>
+
     <div class="maindisplayDiv">
         <div class="insideDisplayDiv">
             <!-- Title above the slideshow -->
@@ -52,7 +55,7 @@
         </div>
     </div>
 
-    <?php include 'sidebar.php' ?>
+
 
     <script>
         let slideIndex = 0;
@@ -74,16 +77,16 @@
             let slides = document.getElementsByClassName("mySlides");
             let dots = document.getElementsByClassName("dot");
             for (i = 0; i < slides.length; i++) {
-                slides[i].style.display = "none";  
+                slides[i].style.display = "none";
             }
             slideIndex++;
             if (slideIndex > slides.length) {
                 slideIndex = 1
-            }    
+            }
             for (i = 0; i < dots.length; i++) {
                 dots[i].className = dots[i].className.replace(" active", "");
             }
-            slides[slideIndex - 1].style.display = "block";  
+            slides[slideIndex - 1].style.display = "block";
             dots[slideIndex - 1].className += " active";
             setTimeout(showSlidesAuto, 3000); // Change slide every 3 seconds
         }
