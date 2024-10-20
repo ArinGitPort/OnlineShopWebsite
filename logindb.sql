fe7df5fc9b2c889f580fcb75a5bf06086c6632bb
ArinGitPort <allenportgithub@gmail.com>
2024-10-20 23:05:22 +0800

    Added New Database

--- logindb.sql
+++ logindb.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: Oct 16, 2024 at 07:26 AM
+-- Generation Time: Oct 20, 2024 at 05:05 PM
 -- Server version: 10.4.32-MariaDB
 -- PHP Version: 8.2.12
 
@@ -31,24 +31,10 @@ CREATE TABLE `addeditem` (
   `id` int(11) NOT NULL,
   `productname` varchar(80) NOT NULL,
   `qty` int(100) NOT NULL,
-  `price` int(100) NOT NULL,
+  `price` decimal(65,2) NOT NULL,
+  `category` varchar(100) NOT NULL,
   `dateadded` timestamp NOT NULL DEFAULT current_timestamp() ON UPDATE current_timestamp()
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
-
---
--- Dumping data for table `addeditem`
---
-
-INSERT INTO `addeditem` (`id`, `productname`, `qty`, `price`, `dateadded`) VALUES
-(11, 'Clay2', 123, 123, '2024-10-11 18:06:30'),
-(12, 'alleb2', 123, 132, '2024-10-11 18:06:30'),
-(13, 'asd', 123, 123, '2024-10-11 18:08:24'),
-(14, 'sdg', 123, 3, '2024-10-11 18:08:30'),
-(15, 'rfg', 123, 45, '2024-10-11 18:08:35'),
-(16, 'asd', 213, 2, '2024-10-11 18:16:24'),
-(17, 'gfdf', 2, 21, '2024-10-13 15:43:39'),
-(18, 'cfd', 2, 3, '2024-10-13 15:43:45'),
-(19, 'Clay', 2, 0, '2024-10-14 07:51:29');
 
 -- --------------------------------------------------------
 
@@ -59,30 +45,11 @@ INSERT INTO `addeditem` (`id`, `productname`, `qty`, `price`, `dateadded`) VALUE
 CREATE TABLE `deleteditem` (
   `id` int(11) NOT NULL,
   `deletedproduct` varchar(80) NOT NULL,
+  `qty` int(11) NOT NULL,
+  `price` decimal(65,2) NOT NULL,
+  `category` varchar(100) NOT NULL,
   `datedeleted` timestamp NOT NULL DEFAULT current_timestamp() ON UPDATE current_timestamp()
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
-
---
--- Dumping data for table `deleteditem`
---
-
-INSERT INTO `deleteditem` (`id`, `deletedproduct`, `datedeleted`) VALUES
-(2, 'bread', '0000-00-00 00:00:00'),
-(3, 'Gun', '2024-10-11 17:22:07'),
-(4, 'Clay', '2024-10-11 17:22:26'),
-(5, 'hello', '2024-10-11 17:48:14'),
-(6, 'Clay', '2024-10-11 17:26:19'),
-(7, 'Toy', '2024-10-11 17:51:46'),
-(8, 'hello', '2024-10-11 17:53:51'),
-(9, 'alleb', '2024-10-11 18:03:25'),
-(10, 'Clay2', '2024-10-11 18:03:26'),
-(11, 'Clay2', '2024-10-11 18:03:28'),
-(12, 'alleb2', '2024-10-11 18:08:38'),
-(13, 'asd', '2024-10-11 18:08:39'),
-(14, 'sdg', '2024-10-11 18:08:39'),
-(15, 'rfg', '2024-10-11 18:08:40'),
-(16, 'asd', '2024-10-14 07:50:11'),
-(18, 'cfd', '2024-10-14 07:50:16');
 
 -- --------------------------------------------------------
 
@@ -94,7 +61,7 @@ CREATE TABLE `inventory` (
   `id` int(11) NOT NULL,
   `productname` varchar(80) NOT NULL,
   `qty` int(255) NOT NULL,
-  `price` int(255) NOT NULL,
+  `price` decimal(65,2) NOT NULL,
   `category` varchar(60) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
@@ -103,8 +70,10 @@ CREATE TABLE `inventory` (
 --
 
 INSERT INTO `inventory` (`id`, `productname`, `qty`, `price`, `category`) VALUES
-(17, 'gfdf', 2, 21, ''),
-(19, 'Clay', 2, 0, '');
+(1, 'Cappybara Bunni Charm', 100, 28.00, 'Bunni Charms'),
+(2, 'Mia Bunny Charm', 100, 20.00, 'Bunni Charms'),
+(3, 'Totoro Phone/Key or Bag Charm', 30, 10.00, 'Phone Strap / Bag Charms'),
+(4, 'Sanrio Phone/Key or Bag Charm', 27, 10.00, 'Phone Strap / Bag Charms');
 
 -- --------------------------------------------------------
 
@@ -116,11 +85,23 @@ CREATE TABLE `orderhistory` (
   `id` int(11) NOT NULL,
   `productname` varchar(100) NOT NULL,
   `qty` int(50) NOT NULL,
-  `price` int(50) NOT NULL,
+  `price` decimal(65,2) NOT NULL,
   `category` varchar(100) NOT NULL,
   `customername` varchar(100) NOT NULL,
   `datecompleted` timestamp NOT NULL DEFAULT current_timestamp() ON UPDATE current_timestamp()
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Dumping data for table `orderhistory`
+--
+
+INSERT INTO `orderhistory` (`id`, `productname`, `qty`, `price`, `category`, `customername`, `datecompleted`) VALUES
+(12, 'Stickers', 20, 235.34, 'Deco Stickers', 'Allen', '2024-10-18 14:26:51'),
+(13, 'Controller', 23, 32.00, 'General Goods', 'Allen', '2024-10-18 14:27:15'),
+(14, 'Clay', 23, 225.00, 'General Goods', 'Allen', '2024-10-18 14:30:23'),
+(15, 'Bag', 2, 235.00, 'General Goods', 'Allen', '2024-10-18 14:30:24'),
+(16, 'Clay3', 123, 123.00, 'General Goods', 'Joshua', '2024-10-18 14:31:03'),
+(17, 'Clay2', 123, 123.00, 'General Goods', 'Allen', '2024-10-19 09:20:37');
 
 -- --------------------------------------------------------
 
@@ -132,7 +113,7 @@ CREATE TABLE `productorder` (
   `id` int(11) NOT NULL,
   `productname` varchar(50) NOT NULL,
   `qty` int(100) NOT NULL,
-  `price` int(100) NOT NULL,
+  `price` decimal(65,2) NOT NULL,
   `category` varchar(50) NOT NULL,
   `customername` varchar(100) NOT NULL,
   `dateadded` timestamp NOT NULL DEFAULT current_timestamp() ON UPDATE current_timestamp()
@@ -143,25 +124,7 @@ CREATE TABLE `productorder` (
 --
 
 INSERT INTO `productorder` (`id`, `productname`, `qty`, `price`, `category`, `customername`, `dateadded`) VALUES
-(1, 'Clay', 20, 100, 'Bunni Charms', 'Allen', '2024-10-15 14:55:30'),
-(2, 'Toy', 2, 50, 'Bunni Charms', 'Allen', '2024-10-15 14:58:10'),
-(3, 'Controller', 30, 200, 'Phone Strap', 'Allen', '2024-10-15 15:01:24'),
-(4, 'Bag', 30, 200, 'Phone Strap', 'Allen', '2024-10-15 15:04:21'),
-(5, 'Bag', 30, 200, 'Phone Strap', 'Allen', '2024-10-15 15:06:10'),
-(6, 'Bag', 30, 200, 'Phone Strap', 'Allen', '2024-10-15 15:07:03'),
-(7, 'Bag', 30, 200, 'Phone Strap', 'Allen', '2024-10-15 15:08:58'),
-(8, 'Bag', 30, 200, 'Phone Strap', 'Allen', '2024-10-15 15:09:20'),
-(9, 'Food', 4, 34, 'Phone Strap', 'Allen', '2024-10-15 15:17:00'),
-(10, 'Chao Fan', 4, 34, 'Phone Strap', 'Allen', '2024-10-15 15:17:29'),
-(11, 'Aqua Flask', 2, 123, '', 'Allen', '2024-10-15 15:33:00'),
-(12, 'Clay', 2, 3, '', 'Allen', '2024-10-15 15:33:22'),
-(13, 'Clay2', 1, 2, '', 'Allen', '2024-10-15 15:33:36'),
-(14, 'Bag', 1, 3, 'Boxes and Bundles', 'Allen', '2024-10-15 15:36:05'),
-(15, 'Toy', 23, 60, 'General Goods', 'Jeremiah', '2024-10-16 05:07:14'),
-(16, 'Controller', 10, 200, 'General Goods', 'Jorge', '2024-10-16 05:11:40'),
-(17, 'Laptop', 5, 2000, 'Boxes and Bundles', 'Joshua', '2024-10-16 05:15:11'),
-(18, 'Iphone', 10, 300, 'General Goods', 'Joshua', '2024-10-16 05:15:41'),
-(19, 'Zeph', 10, 20, 'General Goods', 'Jose', '2024-10-16 05:23:07');
+(1, 'Sanrio Phone/Key or Bag Charm', 3, 10.00, 'Bunni Charms', 'Allen', '2024-10-20 14:21:41');
 
 -- --------------------------------------------------------
 
@@ -172,19 +135,26 @@ INSERT INTO `productorder` (`id`, `productname`, `qty`, `price`, `category`, `cu
 CREATE TABLE `userinfo` (
   `id` int(11) NOT NULL,
   `username` varchar(50) NOT NULL,
-  `password` varchar(50) NOT NULL
+  `userpassword` varchar(255) NOT NULL,
+  `useremail` varchar(100) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
 --
 -- Dumping data for table `userinfo`
 --
 
-INSERT INTO `userinfo` (`id`, `username`, `password`) VALUES
-(1, 'admin', 'admin');
+INSERT INTO `userinfo` (`id`, `username`, `userpassword`, `useremail`) VALUES
+(1, 'admin', '$2y$10$qHIEEuTx2E8xfLM7LN8YYOIdSiheOCJNnzhzOwhw3/mSYg6zZG4Hy', 'admin@gmail.com');
 
 --
 -- Indexes for dumped tables
 --
+
+--
+-- Indexes for table `addeditem`
+--
+ALTER TABLE `addeditem`
+  ADD PRIMARY KEY (`id`);
 
 --
 -- Indexes for table `deleteditem`
@@ -221,28 +191,34 @@ ALTER TABLE `userinfo`
 --
 
 --
+-- AUTO_INCREMENT for table `addeditem`
+--
+ALTER TABLE `addeditem`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+
+--
 -- AUTO_INCREMENT for table `deleteditem`
 --
 ALTER TABLE `deleteditem`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=19;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
 
 --
 -- AUTO_INCREMENT for table `inventory`
 --
 ALTER TABLE `inventory`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=20;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
 
 --
 -- AUTO_INCREMENT for table `orderhistory`
 --
 ALTER TABLE `orderhistory`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=20;
 
 --
 -- AUTO_INCREMENT for table `productorder`
 --
 ALTER TABLE `productorder`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=20;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
 
 --
 -- AUTO_INCREMENT for table `userinfo`
