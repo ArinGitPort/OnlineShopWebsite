5a7150c2c542ebec69f3335ac00aa2db03220133
Allen <rimuesencho@gmail.com>
2025-03-22 01:37:21 +0800

    changed layout

--- home.php
+++ home.php
@@ -1,98 +1,111 @@
-<?php
-
-include 'sessionchecker.php';
-?>
-
 <!DOCTYPE html>
 <html lang="en">
-
 <head>
-    <link rel="stylesheet" href="stylingfile/maindisplay.css">
-    <link rel="stylesheet" href="stylingfile/home.css">
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="icon" href="iconlogo/bunniwinkleIcon.ico">
-    <title>Inventory</title>
+  <meta charset="UTF-8">
+  <title>Home</title>
+
+  <!-- layout.css for overall layout (sidebar + main) -->
+  <link rel="stylesheet" href="stylingfile/layout.css">
+  <!-- home.css for slideshow or home-specific styles -->
+  <link rel="stylesheet" href="stylingfile/home.css">
 </head>
-
 <body>
+  <div class="pageWrapper">
+    <!-- Sidebar -->
+    <?php include 'sidebar.php'; ?>
 
-    <?php include 'sidebar.php' ?>
+    <!-- Outer container for the main display area -->
+    <div class="mainDisplayContainer">
+      <div class="mainDisplay">
+        <!-- The white box container -->
+        <div class="homeContainer">
+          <h2 class="slideshow-title">WELCOME TO BUNNIWINKLE INVENTORY SYSTEM</h2>
 
-    <div class="maindisplayDiv2">
-        <div class="insideDisplayDiv2">
-
-            <h2 class="slideshow-title">WELCOME TO BUNNIWINKLE INVENTORY SYSTEM</h2>
-
-            <div class="slideshow-container">
-
-                <div class="mySlides fade">
-                    <div class="numbertext">1 / 3</div>
-                    <img src="imgBG/bunniBG.jpg" class="slideshow-image">
-                    <div class="text"></div>
-                </div>
-
-                <div class="mySlides fade">
-                    <div class="numbertext">2 / 3</div>
-                    <img src="imgBG/bunniliveshop.jpg" class="slideshow-image">
-                    <div class="text"></div>
-                </div>
-
-                <div class="mySlides fade">
-                    <div class="numbertext">3 / 3</div>
-                    <img src="imgBG/bunniartscraft.jpg" class="slideshow-image">
-                    <div class="text"></div>
-                </div>
-                <p class="introducePara">Welcome to bunniwinkle inventory system app. The goal of this app is to make
-                    monitoring of orders and products easier. </p>
-
-                <a class="prev" onclick="plusSlides(-1)">&#10094;</a>
-                <a class="next" onclick="plusSlides(1)">&#10095;</a>
+          <div class="slideshow-container">
+            <div class="mySlides fade">
+              <div class="numbertext">1 / 3</div>
+              <img src="imgBG/bunniBG.jpg" class="slideshow-image">
             </div>
-            <br>
-
-            <div style="text-align:center">
-                <span class="dot" onclick="currentSlide(1)"></span>
-                <span class="dot" onclick="currentSlide(2)"></span>
-                <span class="dot" onclick="currentSlide(3)"></span>
+            <div class="mySlides fade">
+              <div class="numbertext">2 / 3</div>
+              <img src="imgBG/bunniliveshop.jpg" class="slideshow-image">
             </div>
-        </div>
-    </div>
+            <div class="mySlides fade">
+              <div class="numbertext">3 / 3</div>
+              <img src="imgBG/bunniartscraft.jpg" class="slideshow-image">
+            </div>
 
+            <p class="introducePara">
+              Welcome to bunniwinkle inventory system app. The goal of this app is 
+              to make monitoring of orders and products easier.
+            </p>
 
+            <!-- Next/Prev Arrows -->
+            <a class="prev" onclick="plusSlides(-1)">&#10094;</a>
+            <a class="next" onclick="plusSlides(1)">&#10095;</a>
+          </div>
 
-    <script>
-        let slideIndex = 0;
-        showSlidesAuto(); 
+          <!-- Dots Indicators -->
+          <div style="text-align:center">
+            <span class="dot" onclick="currentSlide(1)"></span>
+            <span class="dot" onclick="currentSlide(2)"></span>
+            <span class="dot" onclick="currentSlide(3)"></span>
+          </div>
+        </div> <!-- end .homeContainer -->
+      </div>   <!-- end .mainDisplay -->
+    </div>     <!-- end .mainDisplayContainer -->
+  </div>       <!-- end .pageWrapper -->
 
-      
-        function plusSlides(n) {
-            showSlides(slideIndex += n);
-        }
+  <script>
+    let slideIndex = 0;
+    showSlidesAuto();
 
-        function currentSlide(n) {
-            showSlides(slideIndex = n - 1);
-        }
-        function showSlidesAuto() {
-            let i;
-            let slides = document.getElementsByClassName("mySlides");
-            let dots = document.getElementsByClassName("dot");
-            for (i = 0; i < slides.length; i++) {
-                slides[i].style.display = "none";
-            }
-            slideIndex++;
-            if (slideIndex > slides.length) {
-                slideIndex = 1
-            }
-            for (i = 0; i < dots.length; i++) {
-                dots[i].className = dots[i].className.replace(" active", "");
-            }
-            slides[slideIndex - 1].style.display = "block";
-            dots[slideIndex - 1].className += " active";
-            setTimeout(showSlidesAuto, 3000); 
-        }
-    </script>
+    function plusSlides(n) {
+      showSlides(slideIndex += n);
+    }
 
+    function currentSlide(n) {
+      showSlides(slideIndex = n - 1);
+    }
+
+    function showSlidesAuto() {
+      let i;
+      let slides = document.getElementsByClassName("mySlides");
+      let dots = document.getElementsByClassName("dot");
+      for (i = 0; i < slides.length; i++) {
+        slides[i].style.display = "none";
+      }
+      slideIndex++;
+      if (slideIndex > slides.length) {
+        slideIndex = 1;
+      }
+      for (i = 0; i < dots.length; i++) {
+        dots[i].className = dots[i].className.replace(" active", "");
+      }
+      slides[slideIndex - 1].style.display = "block";
+      dots[slideIndex - 1].className += " active";
+      setTimeout(showSlidesAuto, 3000);
+    }
+
+    function showSlides(n) {
+      let i;
+      let slides = document.getElementsByClassName("mySlides");
+      let dots = document.getElementsByClassName("dot");
+      if (n > slides.length) {
+        slideIndex = 1;
+      }
+      if (n < 1) {
+        slideIndex = slides.length;
+      }
+      for (i = 0; i < slides.length; i++) {
+        slides[i].style.display = "none";
+      }
+      for (i = 0; i < dots.length; i++) {
+        dots[i].className = dots[i].className.replace(" active", "");
+      }
+      slides[slideIndex - 1].style.display = "block";
+      dots[slideIndex - 1].className += " active";
+    }
+  </script>
 </body>
-
 </html>
--- sidebar.php
+++ sidebar.php
@@ -1,71 +1,68 @@
 <link rel="stylesheet" href="stylingfile/sidebarstyle.css">
 
-<div class="dashboardContainer">
-    <div class="dashboardProfile">
-        <div>
-            <h1 class="inventoryLabel">INVENTORY</h1>
-        </div>
-        <div><img class="userIcon" src="imgBG/bunniwinkelanotherlogo.jpg" alt=""></div>
-    </div>
+<div class="pageWrapper">
+  <!-- Sidebar -->
+ <!-- sidebar.php -->
+<aside class="sidebarContainer">
+  <div class="dashboardProfile">
+    <img class="userIcon" src="imgBG/bunniwinkelanotherlogo.jpg" alt="User Icon">
+  </div>
 
-    <div class="sidebarMenuContainer">
-        <ul class="listDashboard">
-            <li>
-                <div class="listDiv">
-                    <img src="iconlogo/home.png" class="imgIcon">
-                    <a href="home.php" class="menuDashboard">HOME</a>
-                </div>
-            </li>
-            <li>
-                <div class="listDiv">
-                    <img src="iconlogo/order.png" class="imgIcon">
-                    <a href="productorder.php" class="menuDashboard">ORDER</a>
-                </div>
-            </li>
-            <li>
-                <div class="listDiv">
-                    <img src="iconlogo/shopping-list.png" class="imgIcon">
-                    <a href="inventory.php" class="menuDashboard">PRODUCTS</a>
-                </div>
-            </li>
-            <li>
-                <div class="listDiv">
-                    <img src="iconlogo/trend.png" class="imgIcon">
-                    <a href="graph.php" class="menuDashboard">GRAPH</a>
-                </div>
-            </li>
-            <li class="dropdown">
-                <div class="listDiv dropdown-container">
-                    <img src="iconlogo/history.png" class="imgIcon">
-                    <a href="javascript:void(0);" class="menuDashboard" onclick="toggleDropdown()">HISTORY</a>
-                    <ul class="dropdown-content" id="dropdownContent">
-                        <li><a href="added_items.php" class="dropdown-item">Added Products</a></li>
-                        <li><a href="deleted_items.php" class="dropdown-item">Unavailable Products</a></li>
-                        <li><a href="permdeleted_items.php" class="dropdown-item">Deleted Products</a></li>
-                        <li><a href="order_history.php" class="dropdown-item">Completed Order</a></li>
-                    </ul>
-                </div>
-            </li>
-            <li>
-                <div class="listDiv">
-                    <img src="iconlogo/logout.png" class="imgIcon">
-                    <a href="logout.php" class="menuDashboard">LOG-OUT</a>
-                </div>
-            </li>
-        </ul>
-    </div>
-</div>
-
-
-
+  <ul class="listDashboard">
+    <li>
+      <div class="listDiv">
+        <img src="iconlogo/home.png" class="imgIcon">
+        <a href="home.php" class="menuDashboard">HOME</a>
+      </div>
+    </li>
+    <li>
+      <div class="listDiv">
+        <img src="iconlogo/order.png" class="imgIcon">
+        <a href="productorder.php" class="menuDashboard">ORDER</a>
+      </div>
+    </li>
+    <li>
+      <div class="listDiv">
+        <img src="iconlogo/shopping-list.png" class="imgIcon">
+        <a href="inventory.php" class="menuDashboard">PRODUCTS</a>
+      </div>
+    </li>
+    <li>
+      <div class="listDiv">
+        <img src="iconlogo/trend.png" class="imgIcon">
+        <a href="graph.php" class="menuDashboard">GRAPH</a>
+      </div>
+    </li>
+    <li class="dropdown">
+      <!-- Clickable dropdown container -->
+      <div class="listDiv dropdown-container" onclick="toggleDropdown()">
+        <img src="iconlogo/history.png" class="imgIcon">
+        <a href="javascript:void(0);" class="menuDashboard">HISTORY</a>
+      </div>
+      <!-- Hidden by default; toggled in JS -->
+      <ul class="dropdown-content" id="dropdownContent">
+        <li><a href="added_items.php" class="dropdown-item">Added Products</a></li>
+        <li><a href="deleted_items.php" class="dropdown-item">Unavailable Products</a></li>
+        <li><a href="permdeleted_items.php" class="dropdown-item">Deleted Products</a></li>
+        <li><a href="order_history.php" class="dropdown-item">Completed Order</a></li>
+      </ul>
+    </li>
+    <li>
+      <div class="listDiv">
+        <img src="iconlogo/logout.png" class="imgIcon">
+        <a href="logout.php" class="menuDashboard">LOG-OUT</a>
+      </div>
+    </li>
+  </ul>
+</aside>
 
 <script>
-    function toggleDropdown() {
-        const dropdownContent = document.getElementById('dropdownContent');
-        if (dropdownContent.style.display === 'block') {
-            dropdownContent.style.display = 'none';
-        } else {
-            dropdownContent.style.display = 'block';
-        }
+  function toggleDropdown() {
+    const dropdownContent = document.getElementById('dropdownContent');
+    if (dropdownContent.style.display === 'block') {
+      dropdownContent.style.display = 'none';
+    } else {
+      dropdownContent.style.display = 'block';
     }
+  }
 </script>
